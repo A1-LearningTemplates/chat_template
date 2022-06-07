682501f6e21c06ece38052e792fe8cccf2677143
ANAS ALKHAMIS <93852315+AnasAlkhamis@users.noreply.github.com>
2022-06-07 22:09:26 +0300

    sending clinet data to frontend

--- backend/controllers/chat.js
+++ backend/controllers/chat.js
@@ -2,11 +2,23 @@ const server = require("../index");
 const socket = require("socket.io");
 
 const io = socket(server, { cors: { origin: "http://localhost:3000" } });
-
+const sessionID = [];
 io.on("connection", (socket) => {
+  const data = {
+    id: socket.id,
+    index: sessionID.length,
+  };
+  sessionID.push(data);
+  // console.log(data);
+  socket.emit("connected", data);
+  socket.on("disconnect", (data) => {
+    sessionID.splice(data.index, 1);
+
+    console.log(sessionID, "array");
+  });
+
+  console.log(sessionID);
   socket.on("message", (data) => {
     console.log(data);
-    
   });
-  console.log(`${socket.id} is connected`);
 });
--- client/src/conponents/chat/index.js
+++ client/src/conponents/chat/index.js
@@ -3,8 +3,12 @@ import { io } from "socket.io-client";
 const socket = io("http://localhost:5000");
 const Chat = () => {
   const [message, setMessage] = useState("");
+  const [clientData, setclientData] = useState(null);
   useEffect(() => {
-    socket.on("connection", () => {});
+    socket.on("connected", (data) => {
+      setclientData(data);
+    });
+    return () => socket.emit("disconnect", clientData);
   }, []);
 
   const sendMessage = (e) => {
@@ -13,17 +17,22 @@ const Chat = () => {
     setMessage("");
   };
   return (
-    <form onSubmit={sendMessage}>
-      <button>send</button>
-      <input
-        type="text"
-        placeholder="Message...."
-        value={message}
-        onChange={(e) => {
-          setMessage(e.target.value);
-        }}
-      />
-    </form>
+    <>
+      <div>
+        <h1>Chat APP</h1>
+      </div>
+      <form onSubmit={sendMessage}>
+        <button>send</button>
+        <input
+          type="text"
+          placeholder="Message...."
+          value={message}
+          onChange={(e) => {
+            setMessage(e.target.value);
+          }}
+        />
+      </form>
+    </>
   );
 };
 
