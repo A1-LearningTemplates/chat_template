0b40fdbdcddebb8287f7d9df7f5db27bd12846a7
ANAS ALKHAMIS <93852315+AnasAlkhamis@users.noreply.github.com>
2022-06-07 12:12:44 +0300

    starter code react-app =< client

--- backend/controllers/chat.js
+++ backend/controllers/chat.js
@@ -1,4 +1,9 @@
 const io = require("../index");
 io.on("connection", (socket) => {
   console.log("hello");
+  socket.on("disconnect",()=> {
+  console.log("bye");
+
+  })
 });
+console.log(io);
--- backend/index.js
+++ backend/index.js
@@ -5,9 +5,10 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
-const port = 3000;
+const port = 5000;
 
 server.listen(port, () =>
   console.log(`Example app listening on port ${port}!`)
 );
+
 module.exports = io;
